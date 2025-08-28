f1ad56cce3e83b9c915deb648977b6a957f2c7fe
Itheff <bnagyva@gmail.com>
2025-08-28 00:41:30 -0400

    Restructuring and slash commands testing

--- bot.py
+++ bot.py
@@ -1,10 +1,29 @@
+# My database server id: 770442539760746526
+# Aeronautica ATC server id: 1120012114321494088
+
 import discord
+import time
 
-class AeronauticaATCBot(discord.Client):
+intents = discord.Intents.default()
+intents.message_content = True
 
-    def __init__(self, intents):
-        super().__init__(intents=intents)
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
 
-    async def on_ready(self):
-        print("Logged in as")
-        print(self.user.name)
+@tree.command(name="Ping", description="Ping the bot and it will ping back", guilds=[770442539760746526, 1120012114321494088])
+async def ping(interaction):
+    await interaction.response.send_massage("Ping!")
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+    await tree.sync(guild=discord.Object(id=770442539760746526))
+    print("Commands loaded for testing server")
+    await tree.sync(guild=discord.Object(id=1120012114321494088))
+    print("Commands loaded for Aeronautica ATC server")
+
+    print("Loading complete, bot is online")
+
+file = open("token.txt", "r")
+token = file.read()
+client.run(token=token)

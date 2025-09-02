ff2801ba730b1c5491a8e4e81a6c656f147e0125
Itheff <bnagyva@gmail.com>
2025-09-02 18:18:10 -0400

    Added documentation and squawk command

--- bot.py
+++ bot.py
@@ -14,21 +14,44 @@ guilds: list = [discord.Object(id=770442539760746526), discord.Object(id=1120012
 
 bot: commands.Bot = commands.Bot(command_prefix="!", intents=intents)
 
+"""
+This command sends "Pong!" to the same channel that the command is run in
+"""
 @bot.tree.command(name="ping", description="Ping the bot and it will ping back", guilds=guilds)
 async def ping(ctx: discord.Interaction):
     await ctx.response.send_message("Pong!")
 
+"""
+This command is a testing command for parameters testing
+"""
 @bot.tree.command(name="paramtest", description="Testing for parameters", guilds=guilds)
 async def paramtest(ctx: discord.Interaction, arg: str):
     await ctx.response.send_message(f"{arg}")
 
+"""
+This command takes an airport code and returns all positions that could control that airport WIP
+"""
 @bot.tree.command(name="positions", description="Find all the positions that control an airport", guilds=guilds)
 async def find_frequency(ctx: discord.Interaction, airport: str):
     await ctx.response.send_message(bot_functions.find_frequency(airport))
 
+"""
+This command returns the time in UTC in the form HHMMz
+"""
 @bot.tree.command(name="utc", description="Find the current time in UTC", guilds=guilds)
 async def send_time_utc(ctx: discord.Interaction):
     await ctx.response.send_message(bot_functions.get_time_utc())
+
+"""
+This command returns a random squawk code, does not check for invalid codes
+"""
+@bot.tree.command(name="squawk", description="Generate a random sqawk code", guilds=guilds)
+async def squawk(ctx: discord.Interaction):
+    await ctx.response.send_message(bot_functions.generate_squawk())
+
+@bot.tree.command(name="generate_atis", description="Generates an ATIS from input information", guilds=guilds)
+async def gen_atis(ctx: discord.Interaction):
+    await ctx.response.send_message(bot_functions.generate_atis())
 
 @bot.event
 async def on_ready():
--- bot_functions.py
+++ bot_functions.py
@@ -1,4 +1,5 @@
 import time
+from random import randint
 
 def find_frequency(airport: str) -> str:
     if airport.upper() == "KCIA":
@@ -8,3 +9,16 @@ def find_frequency(airport: str) -> str:
     
 def get_time_utc() -> str:
     return time.strftime("%H%MZ", time.gmtime(time.time()))
+
+def generate_squawk() -> str:
+    squawk: str = ""
+    for i in range(0,4):
+        squawk = squawk + str(randint(0,7))
+    return squawk
+
+def generate_atis() -> str:
+    atis: str = ""
+
+    atis = atis + "`test`"
+
+    return atis

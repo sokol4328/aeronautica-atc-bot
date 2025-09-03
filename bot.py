793ba4773d4ed58c22be296ecf3cdc44386cc993
Itheff <bnagyva@gmail.com>
2025-09-03 00:20:19 -0400

    New commands and ATIS stuff

--- bot.py
+++ bot.py
@@ -4,35 +4,40 @@
 import discord
 from discord.ext import commands
 import time
-import typing
+import threading
+from typing import List
 import bot_functions
+import pickle
 
 intents = discord.Intents.default()
 intents.message_content = True
-
-guilds: list = [discord.Object(id=770442539760746526), discord.Object(id=1120012114321494088)]
-
+guilds: list = [discord.Object(id=1120012114321494088)] #discord.Object(id=770442539760746526),]
 bot: commands.Bot = commands.Bot(command_prefix="!", intents=intents)
+
+def check_permissions(ctx: discord.Interaction, roles: List[int]):
+    user_roles: List[discord.Role] = ctx.user.roles # type: ignore
+    for role in roles:
+        for user_role in user_roles:
+            if user_role.id == role:
+                return True
+    return False
 
 """
 This command sends "Pong!" to the same channel that the command is run in
 """
 @bot.tree.command(name="ping", description="Ping the bot and it will ping back", guilds=guilds)
 async def ping(ctx: discord.Interaction):
+    if not check_permissions(ctx, [1175139363961712730]):
+        await ctx.response.send_message("You do not have permission to use this command", ephemeral=True)
     await ctx.response.send_message("Pong!")
-
-"""
-This command is a testing command for parameters testing
-"""
-@bot.tree.command(name="paramtest", description="Testing for parameters", guilds=guilds)
-async def paramtest(ctx: discord.Interaction, arg: str):
-    await ctx.response.send_message(f"{arg}")
 
 """
 This command takes an airport code and returns all positions that could control that airport WIP
 """
 @bot.tree.command(name="positions", description="Find all the positions that control an airport", guilds=guilds)
 async def find_frequency(ctx: discord.Interaction, airport: str):
+    if not check_permissions(ctx, [1175139363961712730]):
+        await ctx.response.send_message("You do not have permission to use this command", ephemeral=True)
     await ctx.response.send_message(bot_functions.find_frequency(airport))
 
 """
@@ -40,6 +45,8 @@ This command returns the time in UTC in the form HHMMz
 """
 @bot.tree.command(name="utc", description="Find the current time in UTC", guilds=guilds)
 async def send_time_utc(ctx: discord.Interaction):
+    if not check_permissions(ctx, [1175139363961712730]):
+        await ctx.response.send_message("You do not have permission to use this command", ephemeral=True)
     await ctx.response.send_message(bot_functions.get_time_utc())
 
 """
@@ -47,15 +54,45 @@ This command returns a random squawk code, does not check for invalid codes
 """
 @bot.tree.command(name="squawk", description="Generate a random sqawk code", guilds=guilds)
 async def squawk(ctx: discord.Interaction):
+    if not check_permissions(ctx, [1175139363961712730]):
+        await ctx.response.send_message("You do not have permission to use this command", ephemeral=True)
     await ctx.response.send_message(bot_functions.generate_squawk())
 
+"""
+THis command makes an ATIS based on user given information. It also stores the generated ATIS via pickle.
+"""
 @bot.tree.command(name="generate_atis", description="Generates an ATIS from input information", guilds=guilds)
-async def gen_atis(ctx: discord.Interaction, airport: str, wind: str, visibility: str, clouds: str, temp: str, dewpoint: str):
-    atis: str = bot_functions.generate_atis(airport, wind, visibility, clouds, temp, dewpoint)
-    if atis[0:3] == "ERR":
-        await ctx.response.send_message(atis[3:])
-    else:
-        await ctx.response.send_message(bot_functions.generate_atis(airport, wind, visibility, clouds, temp, dewpoint))
+async def gen_atis(ctx: discord.Interaction, airport: str, wind: str, temp: str, dewpoint: str, pressure:str, clouds: str, visibility: str):
+    if not check_permissions(ctx, [1175139363961712730]):
+        await ctx.response.send_message("You do not have permission to use this command", ephemeral=True)
+    atis: bot_functions.ATIS = bot_functions.ATIS(airport, wind, temp, dewpoint, pressure, clouds, visibility)
+    try:
+        file = open(f"atis_database/{airport.lower()}.atis", "x")
+        file.close()
+        file = open(f"atis_database/{airport.lower()}.atis", "wb")
+        pickle.dump(obj=atis, file=file)
+        await ctx.response.send_message(atis.to_string())
+    except FileExistsError as e:
+        await ctx.response.send_message(f"An ATIS already exists for {atis.airport.upper()}, try /edit_atis or /delete_atis")
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name="say", description="Says something in the provided channel id", guilds=guilds)
+async def say(ctx: discord.Interaction, message: str, channel_id: str="0"):
+    if not check_permissions(ctx, [1175139363961712730, 1175139363961712730]):
+        await ctx.response.send_message("You do not have permission to use this command", ephemeral=True)
+    try:
+        channel = None
+        if channel_id == "0":
+            channel = ctx.channel # type: ignore
+        else:
+            channel = bot.get_channel(int(channel_id)) # type: ignore
+        await channel.send(message) # type: ignore
+        await ctx.response.send_message(f"Message sent to <#{channel.id}>", ephemeral=True, delete_after=5) # type: ignore
+    except Exception as e:
+        await ctx.response.send_message("An unknown error has occured", ephemeral=True)
+        print(e)
+
 
 @bot.event
 async def on_ready():
--- bot_functions.py
+++ bot_functions.py
@@ -1,18 +1,47 @@
 import time
 from random import randint
+from typing import List
 
 class ATIS:
-    def __init__(self, airport: str, wind: str, temp: str, dewpoint: str):
-        self.id
-        self.airport = airport
+    def __init__(self, airport: str, wind: str, temp: str, dewpoint: str, pressure: str, clouds: str, visibility: str):
+        self.airport: str = airport.upper()
+        self.atis_letter: int = 0
         self.fir: str = get_fir(self.airport)
-        self.wind = wind
-        self.temp = temp
-        self.dewpoint = dewpoint
+        self.wind: str = wind
+        self.temp: str = temp
+        self.dewpoint: str = dewpoint
+        self.pressure: str = pressure
+        self.clouds: str = clouds
+        self.visibility: str = visibility
+        self.approach: str = "VISUAL"
+        self.runways: List[str] = []
+        self.dep_runways: List[str] = []
+        self.dispatch_station: str = "UNICOM"
+        self.dispatch_freq: str = "122.800"
+        self.pdc: str = "UNAVAIL"
+        self.server_code: str = "000000"
+
+    @staticmethod
+    def get_info_letter(info_num: int):
+        return chr(ord('a') + info_num).upper()
     
     def to_string(self):
         if self.fir == "FAA":
-            pass
+            atis: str = "`"
+            atis += f"{self.airport} ATIS INFO {ATIS.get_info_letter(self.atis_letter)} {get_time_utc()}"
+            atis += f"\n{self.wind}KT {self.visibility}SM {self.clouds} {self.temp}/{self.dewpoint} A{self.pressure}"
+            atis += f"\n{self.approach} APCH "
+            for runway in self.runways:
+                atis += f"{runway} "
+            if len(self.dep_runways) != 0:
+                f"\nDEP "
+                for runway in self.dep_runways:
+                    atis += f"{runway} "
+            atis += f"\nREADBACK ALL RUNWAY HOLD SHORT INSTRUCTIONS"
+            atis += f"\nCONTACT {self.dispatch_station} ON {self.dispatch_freq} FOR CLNC INSTRUCTIONS"
+            atis += f"\nTEXT PILOTS USE `<#1253808325129408552>` | PDC {self.pdc}"
+            atis += f"\nADVIS YOU HAVE {ATIS.get_info_letter(self.atis_letter)}`"
+            return atis
         elif self.fir == "CAA":
             pass
         elif self.fir == "ICAO":
@@ -32,20 +61,6 @@ def generate_squawk() -> str:
     for i in range(0,4):
         squawk = squawk + str(randint(0,7))
     return squawk
-
-def generate_atis(airport: str, wind: str, visibility: str, clouds: str, temp: str, dewpoint: str) -> str:
-    atis: str = ""
-    fir: str = get_fir(airport)
-    if fir == "FAA":
-        pass
-    elif fir == "CAA":
-        pass    
-    elif fir == "ICAO":
-        pass
-    else:
-        return "ERRAirport not found"
-    atis += "`test success`"
-    return atis
 
 # TODO
 def get_fir(airport: str):

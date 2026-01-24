0885045a1d7aca776375b1b598a289e134b30382
sokol4328 <pin0609pong@gmail.com>
2026-01-24 21:32:55 +0100

    Now uses .env and has a proper DEBUG log

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from bot import AeroATCBot
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 def main():
     bot = AeroATCBot()
-    file = open("token.txt", "r")
-    token = file.read()
-    bot.run(token)
+    bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
 
 if __name__ == "__main__":
     main()

7ab71e36ad70824fffc4399a490f84ffccf5953e
sokol4328 <pin0609pong@gmail.com>
2026-01-28 08:19:27 +0100

    gitignore cleanup and CL logging

--- aero_atc_bot_functions/misc_commands.py
+++ aero_atc_bot_functions/misc_commands.py
@@ -2,6 +2,7 @@ import discord.app_commands, discord.utils
 from discord import Interaction, ForumChannel, CategoryChannel
 from random import randint
 from .permissions import has_role, RoleIDs
+import datetime
 
 @discord.app_commands.command(description="Ping the bot and it will say \"Pong!\" in response")
 @has_role(RoleIDs.VERIFIED)
@@ -13,14 +14,14 @@ async def ping(ctx: Interaction):
 async def utc(ctx: Interaction):
     await ctx.response.send_message(f"{discord.utils.utcnow().strftime('%I:%MZ')}")
 
-@discord.app_commands.command(description="Generate a 4 digit squawk code. Does not check for invalid codes")
+@discord.app_commands.command(description="Generate a 4 digit squawk code")
 @has_role(RoleIDs.VERIFIED)
 async def generate_squawk(ctx: Interaction):
     squawk: str = ""
     for i in range(4):
         squawk += str(randint(0,7))
     print(f"Generated squawk code: {squawk} by {ctx.user.display_name}")
-    if squawk in {"1200", "7000", "7500", "7600", "7700"}:
+    if squawk in {"1200", "7000", "7500", "7600", "7700", "7777"}:
         await ctx.response.send_message("Unknown error, please try again.", ephemeral=True)
     await ctx.response.send_message(f"Generated squawk code: {squawk}", ephemeral=True)
 
@@ -32,6 +33,7 @@ async def say(ctx: Interaction, message: str):
         if ctx.channel is not None:
             await ctx.response.send_message("Message sucessfully sent", ephemeral=True, delete_after=3)
             await ctx.channel.send(message)
+            print(f"Message sent by {ctx.user.display_name} at {datetime.datetime.now(datetime.timezone.utc)} UTC")
         else:
             await ctx.response.send_message("An unknown error has occured", ephemeral=True)
     else:
